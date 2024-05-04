48cddba6b5555bbaea7644e350fd424030d13d1c
Joshcoderj <152977401+Joshcoderj@users.noreply.github.com>
2024-05-04 10:52:37 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,1 +1,25 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler
 
+TOKEN = 'YOUR_API_TOKEN'
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Hello!')
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    echo_handler = MessageHandler(Filters.text, echo)
+    dispatcher.add_handler(echo_handler)
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
